d6de1b7adb22d0725f06fad8f6bface94272c441
Juan Manuel Canahuate Vargas <juan.manuel.canahuare@gmail.com>
2022-07-07 14:21:51 -0400

    deleted logs

--- app.js
+++ app.js
@@ -64,8 +64,6 @@ var swaggerDocs = swaggerJsDoc(swaggerOptions);
 var app = express();
 
 const PORT = process.env.PORT || 8087;
-console.log(process.env.DATABASE_URL)
-console.log(process.env.URL)
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
