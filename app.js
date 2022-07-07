834c6b2e9b6a2c61a8ea701d4de21abe3ce2158f
Juan Manuel Canahuate Vargas <juan.manuel.canahuare@gmail.com>
2022-07-07 14:16:03 -0400

    port

--- app.js
+++ app.js
@@ -64,7 +64,8 @@ var swaggerDocs = swaggerJsDoc(swaggerOptions);
 var app = express();
 
 const PORT = process.env.PORT || 8087;
-console.log(process.env)
+console.log(process.env.DATABASE_URL)
+console.log(process.env.URL)
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
