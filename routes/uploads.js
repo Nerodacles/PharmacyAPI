efa0812ad1865a3e9031bd1bee72b7197a170832
Nerodacles <juan.manuel.canahuare@gmail.com>
2022-08-27 17:38:22 -0400

    Rate limiting get images

--- routes/uploads.js
+++ routes/uploads.js
@@ -1,6 +1,14 @@
 var express = require('express');
 var router = express.Router();
 var path = require('path');
+const rateLimit = require('express-rate-limit');
+
+const limiter = rateLimit({
+    windowMs: 5 * 60 * 1000, // 15 minutes
+    max: 50 // limit each IP to 50 requests per windowMs
+});
+
+router.use(limiter)
 
 router.get('/:filename', function(req, res, next) {
     res.sendFile(path.join(__dirname, '../uploads' + req.url))
